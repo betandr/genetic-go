2b2dbd7e03bb50305eee88abe59a8e1ce0a9fe9f
Beth Anderson <beth.anderson@bbc.co.uk>
2022-09-02 12:41:16 +0100

    SAVEPOINT

--- genetic/population.go
+++ genetic/population.go
@@ -1,6 +1,10 @@
 package genetic
 
-import "crypto/rand"
+import (
+	"crypto/rand"
+	"fmt"
+	"strings"
+)
 
 type Population struct {
 	Organisms []Organism
@@ -21,6 +25,28 @@ func (p *Population) Populate(size int) {
 	}
 }
 
+func (p *Population) String() string {
+	var sb strings.Builder
+
+	for _, o := range p.Organisms {
+		sb.WriteString(o.String())
+		sb.WriteString(", ")
+	}
+	return fmt.Sprintf("[%s]", strings.TrimRight(sb.String(), ", "))
+}
+
+func (p *Population) AddOrganism(index int, org Organism) {
+	p.Organisms[index] = org
+}
+
 func (p *Population) Evolve(elitist bool, eval Evaluator) *Population {
 	return p // todo
+}
+
+func (p *Population) mutate(org Organism) Organism {
+	return org // todo
+}
+
+func (p *Population) crossover(parent1 Organism, parent2 Organism) Organism {
+	return Organism{}
 }
